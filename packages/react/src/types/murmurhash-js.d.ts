e41879d17b2db98a8ebd8aeb1cbd7bc8016e687f
Ben King <ben@lionfeet.com>
2025-12-25 01:31:02 +0000

    Add types

--- packages/react/src/types/murmurhash-js.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'murmurhash-js' {
-  const murmur: {
-    murmur2(str: string, seed?: number): number;
-    murmur3(str: string, seed?: number): number;
-  };
-  export default murmur;
-}
