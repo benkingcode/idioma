8ed2e0445714f8b8c7843f469b85f97b30764451
Ben King <ben@lionfeet.com>
2026-01-03 11:23:19 +0000

    Use html reporter

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   forbidOnly: CI,
   retries: CI ? 2 : 0,
   workers: CI ? 1 : undefined,
-  reporter: CI
-    ? 'github'
-    : [['json', { outputFile: 'playwright-results.json' }]],
+  reporter: CI ? 'github' : [['html', { open: 'never' }]],
   timeout: 10000,
 
   use: {
